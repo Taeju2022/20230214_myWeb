adef6b8caf3e7486df4fc0076f64556ab5dc72d1
Taeju2022 <thinkhardgogo@gmail.com>
2023-02-16 14:26:19 +0900

    ㅇ
    ㅇ

--- src/kr/member/controller/LogoutController.java
+++ src/kr/member/controller/LogoutController.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LogoutController
  */
+
+
 @WebServlet("/Logout")
 public class LogoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
